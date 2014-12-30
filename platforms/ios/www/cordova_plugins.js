5f6f1838cfd3ef5138478cf8e73839377f323c98
bkarna <bkarna@MacBook-Pro-A1278-1094-Mavericks-2.local>
2014-12-30 18:04:59 +0530

    First Comit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,79 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
+        "id": "org.apache.cordova.battery-status.battery",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
+        "id": "org.apache.cordova.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
+        "id": "org.apache.cordova.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/com.example.custom-camera/www/js/custom_camera.js",
+        "id": "com.example.custom-camera.CustomCamera",
+        "clobbers": [
+            "navigator.CustomCamera"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.statusbar": "0.1.9",
+    "org.apache.cordova.battery-status": "0.2.12",
+    "org.apache.cordova.device": "0.2.13",
+    "org.apache.cordova.device-orientation": "0.3.10",
+    "org.apache.cordova.network-information": "0.2.14",
+    "com.example.custom-camera": "0.0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/ios/www/plugins/com.example.custom-camera/www/js/custom_camera.js
@@ -0,0 +1,7 @@
+cordova.define("com.example.custom-camera.CustomCamera", function(require, exports, module) { var CustomCamera = {
+	getPicture: function(success, failure){
+		cordova.exec(success, failure, "CustomCamera", "openCamera", []);
+	}
+};
+module.exports = CustomCamera;
+});
